89befc4658bcc286bcddcdd548a8435e55a004e8
Prateek-Prakash <63609044+Prateek-Prakash@users.noreply.github.com>
2022-06-18 14:16:08 -0400

    Simplify Endpoints

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import firebase_admin
 import starlette.responses
 
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
 from firebase_admin import credentials
 from firebase_admin import messaging
-
-from models.fcm_message import FCMMessage
-from models.fcm_token import FCMToken
 
 app = FastAPI(title='qTorrentNotifier')
 cred = credentials.Certificate("ServiceAccountKey.json")
@@ -16,22 +14,22 @@ firebase_admin.initialize_app(cred)
 async def redirect_root():
     return starlette.responses.RedirectResponse('/redoc')
 
-@app.post('/api/v1/registerToken')
-async def register_token(fcm_token: FCMToken):
+@app.get('/api/v1/registerToken')
+async def register_token(token: str):
     with open('Token.txt', 'w') as token_file:
-        token_file.write(fcm_token.token)
-    return 'Done'
+        token_file.write(token)
+    return PlainTextResponse('Done')
 
-@app.post('/api/v1/sendMessage')
-async def send_message(fcm_message: FCMMessage):
+@app.get('/api/v1/sendMessage')
+async def send_message(title: str, body: str):
     with open('Token.txt', 'r') as token_file:
         registration_token = token_file.readline()
         message = messaging.Message(
             notification = messaging.Notification(
-                title = fcm_message.title,
-                body = fcm_message.body,
+                title = title,
+                body = body,
             ),
             token = registration_token
         )
         messaging.send(message)
-    return 'Done'
+    return PlainTextResponse('Done')
--- models/fcm_token.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel
-
-class FCMToken(BaseModel):
-    token: str
